3239453c1c70b2e7ebc364eaec0412e88ecc6186
a-scala <alves.scala07@gmail.com>
2026-07-07 14:26:23 +0200

    ex035

---
+++ ex035.py
@@ -0,0 +1,7 @@
+r1 = float(input('Primeira reta: '))
+r2 = float(input('Segunda reta: '))
+r3 = float(input('Terceira reta: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos PODEM FORMAR um triângulo!')
+else:
+    print('Os segmentos NÃO PODEM FORMAR um triângulo.')
